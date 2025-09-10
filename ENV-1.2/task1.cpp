83775dd66264b5abceccdf19f49172b022e83803
Anush <anush2006.5@gmail.com>
2025-09-10 19:12:14 +0400

    dz 2 corrected

--- ENV-1.2/task1.cpp
+++ ENV-1.2/task1.cpp
@@ -1,35 +1,38 @@
 #include <iostream>
 #include <string>
 
+bool check(const std::string& s, int& value) {
+    try {
+        size_t pos;
+        value = std::stoi(s, &pos);
+        return pos == s.size();
+    } 
+    catch (...) {
+        return false;
+    }
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 4) {
+        std::cout << "Usage: " << argv[0] << "int1 int2 int3" << std::endl;
         return 1;
     }
 
-    try {
-        int a = std::stoi(argv[1]);
-        int b = std::stoi(argv[2]);
-        int c = std::stoi(argv[3]);
-
-        int max = a;
-        if (b > max)
-            max = b;
-        if (c > max)
-            max = c;
-
-        int min = a;
-        if (b < min)
-            min = b;
-        if (c < min)
-            min = c;
-
-        std::cout << "min-" << min << ", max-" << max << std::endl;
-    }
-    catch (const std::exception& e) {
-        std::cout << "Invalid input: all arguments must be int" << std::endl;
+    int a, b, c;
+    if (!check(argv[1], a) || !check(argv[2], b) || !check(argv[3], c)) {
+        std::cout << "Invalid input: all arguments must be integers" << std::endl;
         return 1;
     }
 
+    int max = a;
+    if (b > max) max = b;
+    if (c > max) max = c;
+
+    int min = a;
+    if (b < min) min = b;
+    if (c < min) min = c;
+
+    std::cout << "min-" << min << ", max-" << max << std::endl;
     return 0;
 }
 
--- ENV-1.2/task2.cpp
+++ ENV-1.2/task2.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    for (int i = argc - 1; i > 0; --i) {
+    if (argc == 1) {
+        std::cerr << "Usage: " << argv[0] << " args..." << std::endl;
+        return 1;
+    }
+
+    for (int i = argc - 1; i >= 0; --i) {
         std::cout << argv[i] << " ";
     }
     std::cout << std::endl;
+
     return 0;
 }
 
