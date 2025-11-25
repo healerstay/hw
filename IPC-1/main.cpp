58b45e869de69c1bb140b6a6f44b3892aa4b50ec
Anush Zakoyan <anushzakoyan@Book.local>
2025-11-25 15:48:14 +0400

    fix printing registers

--- IPC-1/main.cpp
+++ IPC-1/main.cpp
@@ -1,31 +1,35 @@
-#include <iostream>
+#include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
 #include <pwd.h>
 #include <sys/ucontext.h>
 
 void handler(int, siginfo_t* info, void* context) {
-    std::cout << "Received SIGUSR1 from PID " << info->si_pid
-              << ", UID " << info->si_uid << " (" 
-              << getpwuid(info->si_uid)->pw_name << ")." << std::endl;
+    char buf[256];
 
-    auto uctx = (ucontext_t*)context;
-/*    std::cout << "Registers: EIP=" << uctx->uc_mcontext.gregs[REG_EIP]
-              << ", EAX=" << uctx->uc_mcontext.gregs[REG_EAX]
-              << ", EBX=" << uctx->uc_mcontext.gregs[REG_EBX] << "." << std::endl;
-*/
+    int len = snprintf(buf, sizeof(buf), 
+            "Received SIGUSR1 from process %d executed by %d (%s).\n", 
+            info->si_pid, info->si_uid, getpwuid(info->si_uid)->pw_name );
+    write(STDOUT_FILENO, buf, len);
+
+    ucontext_t* uctx = (ucontext_t*)context;
+    len = snprintf(buf, sizeof(buf),
+            "State of the context: X0 = %llx, X1 = %llx, PC = %llx\n",
+            uctx->uc_mcontext.regs[0], uctx->uc_mcontext.regs[1], uctx->uc_mcontext.pc);
+    write(STDOUT_FILENO, buf, len);
 }
 
 int main() {
-    std::cout << "PID: " << getpid() << "\n";
+    char buf[64];
+    int len = snprintf(buf, sizeof(buf), "PID: %d\n", getpid());
+    write(STDOUT_FILENO, buf, len);
 
-    struct sigaction new_action = {};
-    new_action.sa_sigaction = handler;
-    new_action.sa_flags = SA_SIGINFO;
-    sigaction(SIGUSR1, &new_action, nullptr);
+    struct sigaction sa = {};
+    sa.sa_sigaction = handler;
+    sa.sa_flags = SA_SIGINFO;
+    sigaction(SIGUSR1, &sa, nullptr);
 
     while (true) { sleep(10); }
 
     return 0;
 }
-
